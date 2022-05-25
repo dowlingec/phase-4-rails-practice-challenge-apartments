79789bdc3faf33456494691a2f9106d0958f31c0
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-25 13:57:28 -0400

    apts controller index create

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -3,4 +3,15 @@ class ApartmentsController < ApplicationController
     apartments = Apartment.all
     render json: apartments
   end
+
+  def create
+    apartment = Apartment.find_by(id: params[:id])
+    apartment.create(number: params[:number])
+    render json: apartment
+  end
+
+  private
+  # def apt_params
+  #   params.permit(:number)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/apartments', to: "apartments#index"
+  post '/apartments', to: "apartments#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
