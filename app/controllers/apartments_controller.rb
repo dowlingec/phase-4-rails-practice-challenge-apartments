024ffa4b4dbb8dca763c143e1e1e046e892e4ad0
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-25 14:04:00 -0400

    update tested working

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -9,6 +9,12 @@ class ApartmentsController < ApplicationController
     render json: apartment
   end
 
+  def update
+    apartment = Apartment.find_by(id: params[:id])
+    apartment.update(number: params[:number])
+    render json: apartment
+  end
+
   private
   # def apt_params
   #   params.permit(:number)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/apartments', to: "apartments#index"
   post '/apartments', to: "apartments#create"
+  patch 'apartments/:id', to: "apartments#update"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
