9da4a9be891bd0b00fb8985b39ab8ccdff7ad4f7
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-25 14:34:46 -0400

    tenant validators added

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,4 +1,7 @@
 class Apartment < ApplicationRecord
     has_many :leases
     has_many :tenants
+
+    validates :name, presence: true
+    validates :age, numericality: { :greater_than_or_equal_to 18 }
 end
--- app/models/lease.rb
+++ app/models/lease.rb
@@ -1,4 +1,5 @@
 class Lease < ApplicationRecord
     belongs_to :apartment
     belongs_to :tenant
+    
 end
