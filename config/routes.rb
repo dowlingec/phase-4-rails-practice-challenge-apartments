f8436b91dd2c3f10dcae6f858e3341f71406c050
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-25 14:43:51 -0400

    refactored code to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
-  get '/apartments', to: "apartments#index"
-  post '/apartments', to: "apartments#create"
-  patch 'apartments/:id', to: "apartments#update"
-  delete '/apartments/:id', to: "apartments#destroy"
 
-  get '/tenants', to: "tenants#index"
-  post '/tenants', to: "tenants#create"
-  patch 'tenants/:id', to: "tenants#update"
-  delete '/tenants/:id', to: "tenants#destroy"
+  resources :apartments
+  # get '/apartments', to: "apartments#index"
+  # post '/apartments', to: "apartments#create"
+  # patch 'apartments/:id', to: "apartments#update"
+  # delete '/apartments/:id', to: "apartments#destroy"
+  resources :tenants
+  # get '/tenants', to: "tenants#index"
+  # post '/tenants', to: "tenants#create"
+  # patch 'tenants/:id', to: "tenants#update"
+  # delete '/tenants/:id', to: "tenants#destroy"
 
   #DELTE index once func checked
   # get '/leases', to: "leases#index"
-  post '/leases', to: "leases#create"
-  delete '/leases/:id', to: "leases#destroy"
+  resources :leases, only: [:create, :destroy]
+  # post '/leases', to: "leases#create"
+  # delete '/leases/:id', to: "leases#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
